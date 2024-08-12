a8b13242911156c4491e707bc11710519b75b375
Charles Raj <charles.raj412@gmail.com>
2024-08-12 12:22:17 -0400

    -- shared transactions in jetpack compose -- shared transaction screen

--- app/src/main/java/com/firebase/chat/MainActivity.kt
+++ app/src/main/java/com/firebase/chat/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.firebase.chat
 
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Media
+import android.widget.FrameLayout.LayoutParams
+import android.widget.GridLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.firebase.chat.ui.theme.FirebaseChatApplicationTheme
+import java.net.URL
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +47,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
